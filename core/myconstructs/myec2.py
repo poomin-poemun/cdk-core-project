14076df9a51edd5b64312873bbd123443f725f31
fumihiro.suda <poomin@wolf.email.ne.jp>
2025-03-09 20:14:24 +0900

    minaoshi2

--- core/myconstructs/myec2.py
+++ core/myconstructs/myec2.py
@@ -10,17 +10,7 @@ from aws_cdk.aws_ec2 import (
     CfnVPCGatewayAttachment,
 )
 
-from core.basetypes.myec2 import (
-    MyInternetGatewayIF,
-    MyRouteIF,
-    MyRouteTableIF,
-    MySubnetIF,
-    MySubnetRouteTableAssociationIF,
-    MyVPCGatewayAttachmentIF,
-    MyVPCIF,
-)
 from core.myconstructs.mybase import MyBase
-from core.myconstructs.mytags import MyTags
 
 
 # ==============================
@@ -31,24 +21,13 @@ class MyVpc(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._vpc_(myif=MyVPCIF(**myif), updif=self.myif)
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _vpc_(self, myif: MyVPCIF, updif: dict) -> CfnVPC:
-        tags = MyTags(myif.tags)
-        rsc = CfnVPC(
-            self.obj,
-            myif.construct_id,
-            cidr_block=myif.cidr_block,
-            enable_dns_hostnames=myif.enable_dns_hostnames,
-            enable_dns_support=myif.enable_dns_support,
-            instance_tenancy=myif.instance_tenancy,
-            ipv4_ipam_pool_id=myif.ipv4_ipam_pool_id,
-            ipv4_netmask_length=myif.ipv4_netmask_length,
-            tags=tags,
-        )
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnVPC:
+        rsc = CfnVPC(**rscif)
         # vpc_id
-        updif["vpc_id"] = rsc.attr_vpc_id
+        myif["vpc_id"] = rsc.attr_vpc_id
         return rsc
 
 
@@ -60,34 +39,13 @@ class MySubnet(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._subnet_(myif=MySubnetIF(**myif), updif=self.myif)
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _subnet_(self, myif: MySubnetIF, updif: dict) -> CfnSubnet:
-        tags = MyTags(myif.tags)
-        rsc = CfnSubnet(
-            self.obj,
-            myif.construct_id,
-            vpc_id=myif.vpc_id,
-            assign_ipv6_address_on_creation=myif.assign_ipv6_address_on_creation,
-            availability_zone=myif.availability_zone,
-            availability_zone_id=myif.availability_zone_id,
-            cidr_block=myif.cidr_block,
-            enable_dns64=myif.enable_dns64,
-            enable_lni_at_device_index=myif.enable_lni_at_device_index,
-            ipv4_ipam_pool_id=myif.ipv4_ipam_pool_id,
-            ipv4_netmask_length=myif.ipv4_netmask_length,
-            ipv6_cidr_block=myif.ipv6_cidr_block,
-            ipv6_ipam_pool_id=myif.ipv6_ipam_pool_id,
-            ipv6_native=myif.ipv6_native,
-            ipv6_netmask_length=myif.ipv6_netmask_length,
-            map_public_ip_on_launch=myif.map_public_ip_on_launch,
-            outpost_arn=myif.outpost_arn,
-            private_dns_name_options_on_launch=myif.private_dns_name_options_on_launch,
-            tags=tags,
-        )
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnSubnet:
+        rsc = CfnSubnet(**rscif)
         # subnet_id
-        updif["attr_subnet_id"] = rsc.attr_subnet_id
+        myif["attr_subnet_id"] = rsc.attr_subnet_id
         return rsc
 
 
@@ -99,31 +57,13 @@ class MyRoute(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._route_(myif=MyRouteIF(**myif), updif=self.myif)
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _route_(self, myif: MyRouteIF, updif: dict) -> CfnRoute:
-        rsc = CfnRoute(
-            self.obj,
-            myif.construct_id,
-            route_table_id=myif.route_table_id,
-            carrier_gateway_id=myif.carrier_gateway_id,
-            core_network_arn=myif.core_network_arn,
-            destination_cidr_block=myif.destination_cidr_block,
-            destination_ipv6_cidr_block=myif.destination_ipv6_cidr_block,
-            destination_prefix_list_id=myif.destination_prefix_list_id,
-            egress_only_internet_gateway_id=myif.egress_only_internet_gateway_id,
-            gateway_id=myif.gateway_id,
-            instance_id=myif.instance_id,
-            local_gateway_id=myif.local_gateway_id,
-            nat_gateway_id=myif.nat_gateway_id,
-            network_interface_id=myif.network_interface_id,
-            transit_gateway_id=myif.transit_gateway_id,
-            vpc_endpoint_id=myif.vpc_endpoint_id,
-            vpc_peering_connection_id=myif.vpc_peering_connection_id,
-        )
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnRoute:
+        rsc = CfnRoute(**rscif)
         # gateway_id
-        updif["gateway_id"] = rsc.gateway_id
+        myif["gateway_id"] = rsc.gateway_id
         return rsc
 
 
@@ -135,18 +75,13 @@ class MyInternetGateway(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._internet_gateway_(
-            myif=MyInternetGatewayIF(**myif), updif=self.myif
-        )
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _internet_gateway_(
-        self, myif: MyInternetGatewayIF, updif: dict
-    ) -> CfnInternetGateway:
-        tags = MyTags(myif.tags)
-        rsc = CfnInternetGateway(self.obj, myif.construct_id, tags=tags)
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnInternetGateway:
+        rsc = CfnInternetGateway(**rscif)
         # attr_internet_gateway_id
-        updif["attr_internet_gateway_id"] = rsc.attr_internet_gateway_id
+        myif["attr_internet_gateway_id"] = rsc.attr_internet_gateway_id
         return rsc
 
 
@@ -158,14 +93,13 @@ class MyRouteTable(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._route_table_(myif=MyRouteTableIF(**myif), updif=self.myif)
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _route_table_(self, myif: MyRouteTableIF, updif: dict) -> CfnRouteTable:
-        tags = MyTags(myif.tags)
-        rsc = CfnRouteTable(self.obj, myif.construct_id, vpc_id=myif.vpc_id, tags=tags)
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnRouteTable:
+        rsc = CfnRouteTable(**rscif)
         # attr_route_table_id
-        updif["attr_route_table_id"] = rsc.attr_route_table_id
+        myif["attr_route_table_id"] = rsc.attr_route_table_id
         return rsc
 
 
@@ -177,20 +111,11 @@ class MySubnetRouteTableAssociation(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._subnet_route_table_association_(
-            myif=MySubnetRouteTableAssociationIF(**myif), updif=self.myif
-        )
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _subnet_route_table_association_(
-        self, myif: MySubnetRouteTableAssociationIF, updif: dict
-    ) -> CfnSubnetRouteTableAssociation:
-        rsc = CfnSubnetRouteTableAssociation(
-            self.obj,
-            myif.construct_id,
-            route_table_id=myif.route_table_id,
-            subnet_id=myif.subnet_id,
-        )
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnSubnetRouteTableAssociation:
+        rsc = CfnSubnetRouteTableAssociation(**rscif)
         return rsc
 
 
@@ -202,20 +127,9 @@ class MyVPCGatewayAttachment(MyBase):
         super().__init__(obj, name)
 
     def create(self, myif: dict) -> Any:
-        self.myif = myif
-        return self._vpc_gateway_attachment_(
-            myif=MyVPCGatewayAttachmentIF(**myif), updif=self.myif
-        )
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
 
-    def _vpc_gateway_attachment_(
-        self, myif: MyVPCGatewayAttachmentIF, updif: dict
-    ) -> CfnVPCGatewayAttachment:
-
-        rsc = CfnVPCGatewayAttachment(
-            self.obj,
-            myif.construct_id,
-            vpc_id=myif.vpc_id,
-            internet_gateway_id=myif.internet_gateway_id,
-            vpn_gateway_id=myif.vpn_gateway_id,
-        )
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnVPCGatewayAttachment:
+        rsc = CfnVPCGatewayAttachment(**rscif)
         return rsc
