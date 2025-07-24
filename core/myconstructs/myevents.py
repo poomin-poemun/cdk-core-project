46bd2152033639e087bc60f70f6781459a917efc
fumihiro.suda <poomin@wolf.email.ne.jp>
2025-07-24 22:30:29 +0900

    s3event 2

---
+++ core/myconstructs/myevents.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from aws_cdk.aws_events import CfnRule
+from constructs import Construct
+
+from core.myconstructs.mybase import MyBase
+
+
+# ==============================
+# CfnRule
+# ==============================
+class MyRule(MyBase):
+    def __init__(self, obj: Any, name: str):
+        super().__init__(obj, name)
+
+    def create(self,myif: dict) -> Any:
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
+
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnRule:
+        rsc = CfnRule(**rscif)
+        return rsc
