47df385473a1ed329aaa85c57f96dc2f2bc0e23e
fumihiro.suda <poomin@wolf.email.ne.jp>
2025-07-25 21:26:33 +0900

    s3event 3

---
+++ core/myconstructs/myiam.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from aws_cdk.aws_iam import CfnRole
+from constructs import Construct
+
+from core.myconstructs.mybase import MyBase
+
+
+# ==============================
+# CfnRole
+# ==============================
+class MyRole(MyBase):
+    def __init__(self, obj: Any, name: str):
+        super().__init__(obj, name)
+
+    def create(self, myif: dict) -> Any:
+        # もしmyifの内容を補正する必要があるならここで！
+        return super().create(myif)
+
+    def _rsc_(self, rscif: dict, myif: dict) -> CfnRole:
+        rsc = CfnRole(**rscif)
+        # attr_arn
+        myif["attr_arn"] = rsc.attr_arn
+        return rsc
