8d9edef96ff659ac1dc870c2b73fe7c49abafe0b
fumihiro.suda <poomin@wolf.email.ne.jp>
2025-08-13 13:02:01 +0900

    apigatewaylambda4

--- tests/unit/test_apigw_lambda_stack.py
+++ tests/unit/test_apigw_lambda_stack.py
@@ -1,13 +1,14 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from core.first_vpc_stack import FirstVpcStack
+from core.apigw_lambda_stack import ApigwLambdaStack
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_core_project_tmp/cdk_core_project_tmp_stack.py
-def test_first_vpc_stack():
+def test_apigw_lambda_stack():
     app = core.App()
-    stack = FirstVpcStack(app, "FirstVpcStack")
+    stack = ApigwLambdaStack(app, "ApigwLambdaStack")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
